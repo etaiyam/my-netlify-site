6f9642fd015c71f46665ffb7c83cd82f0562ad3d
etaiyam <etaiyam@gmail.com>
2025-11-06 12:55:09 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';  // We'll create this next
+import { Provider } from 'react-redux';  // If using Redux (from your code)
+import store from './store';  // Redux store
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
