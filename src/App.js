d2db4582b66422f25549cfec33505ee5a263870a
etaiyam <etaiyam@gmail.com>
2025-11-06 12:56:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+// Import your components, e.g., import OurTeam from './components/OurTeam';
+// Add routes if using React Router
 
+function App() {
+  return (
+    <div className="App">
+      {/* Your site content, e.g. <OurTeam /> */}
+    </div>
+  );
+}
+
+export default App;
