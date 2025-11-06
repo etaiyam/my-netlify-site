dd454c7fad66f764f83d746a22e529087466facd
etaiyam <etaiyam@gmail.com>
2025-11-06 13:47:19 +0100

    Update boolean.js

--- src/reducers/boolean.js
+++ src/reducers/boolean.js
@@ -1,1 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
 
+const booleanSlice = createSlice({
+  name: 'boolean',
+  initialState: { bool: false },  // false = FR, true = EN
+  reducers: {
+    boolRecup: (state) => {
+      state.bool = !state.bool;  // Toggle
+    }
+  }
+});
+
+export const { boolRecup } = booleanSlice.actions;
+export default booleanSlice.reducer;
